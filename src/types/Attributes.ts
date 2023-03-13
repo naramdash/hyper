6d65a1a652afbd432755a0eda22c86cc2b1becbe
naram.dash <juho_kim@outlook.com>
2023-03-13 17:49:09 +0900

    add class and style

--- src/types/Attributes.ts
+++ src/types/Attributes.ts
@@ -1,8 +1,19 @@
 import type { ReadonlyKeys } from "./utils/ReadonlyKeys";
 import type { FunctionKeys } from "./utils/FunctionKeys";
 
+type ClassAndStyle = {
+  /**
+   * Element's class content attribute. String Literal.
+   */
+  class?: string;
+  /**
+   * Element's style content attribute. String Literal.
+   */
+  style?: string;
+};
+
 type Attributes<T extends Element> = Partial<
-  Omit<T, ReadonlyKeys<T> | FunctionKeys<T>>
+  Omit<T, ReadonlyKeys<T> | FunctionKeys<T> | "className"> & ClassAndStyle
 >;
 
 export type { Attributes };
